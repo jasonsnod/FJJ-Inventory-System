a1bf733940ee21da8e2ec13d27429e7b5c0530f3
Jason <jasonsnod@gmail.com>
2024-01-22 17:28:53 -0500

    JS: Added skeleton to ReceivingView

---
+++ vue/src/views/ReceivingView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="capstone-app">
+    <div id="nav">
+      <router-link
+        v-bind:to="{ name: 'logout' }"
+        v-if="$store.state.token !== ''"
+        >Logout</router-link
+      >
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+/* Global styles can be added here */
+</style>
