041ae3510faf09ab57d263bb1a95f28e60d8af73
Jason <jasonsnod@gmail.com>
2024-01-22 17:25:57 -0500

    JS: Added all the views

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter as createRouter, createWebHistory } from 'vue-router'
 import { useStore } from 'vuex'
 
 // Import components
-import HomeView from '../views/HomeView.vue';
+import DashboardView from '../views/DashboardView.vue';
 import LoginView from '../views/LoginView.vue';
 import LogoutView from '../views/LogoutView.vue';
 import RegisterView from '../views/RegisterView.vue';
@@ -18,8 +18,8 @@ import RegisterView from '../views/RegisterView.vue';
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView,
+    name: 'dashboard',
+    component: DashboardView,
     meta: {
       requiresAuth: true
     }
