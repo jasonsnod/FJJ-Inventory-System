bec2cf41c067b610c505a9c4b4c4ab886d414c67
Jason <jasonsnod@gmail.com>
2024-01-22 17:37:21 -0500

    JS: Added path for PickingView and PurchasingView

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,17 +1,17 @@
-import { createRouter as createRouter, createWebHistory } from 'vue-router'
-import { useStore } from 'vuex'
+import { createRouter as createRouter, createWebHistory } from "vue-router";
+import { useStore } from "vuex";
 
 // Import components
-import DashboardView from '../views/DashboardView.vue';
-import LoginView from '../views/LoginView.vue';
-import LogoutView from '../views/LogoutView.vue';
-import RegisterView from '../views/RegisterView.vue';
-import InventoryView from '../views/InventoryView.vue';
-import PickingView from '../views/PickingView.vue';
-import PurchasingView from '../views/PurchasingView.vue';
-import ReceivingView from '../views/ReceivingView.vue';
-import ReturnsView from '../views/ReturnsView.vue';
-import ShippingView from '../views/ShippingView.vue';
+import DashboardView from "../views/DashboardView.vue";
+import LoginView from "../views/LoginView.vue";
+import LogoutView from "../views/LogoutView.vue";
+import RegisterView from "../views/RegisterView.vue";
+import InventoryView from "../views/InventoryView.vue";
+import PickingView from "../views/PickingView.vue";
+import PurchasingView from "../views/PurchasingView.vue";
+import ReceivingView from "../views/ReceivingView.vue";
+import ReturnsView from "../views/ReturnsView.vue";
+import ShippingView from "../views/ShippingView.vue";
 
 /**
  * The Vue Router is used to "direct" the browser to render a specific view component
@@ -23,64 +23,79 @@ import ShippingView from '../views/ShippingView.vue';
  */
 const routes = [
   {
-    path: '/',
-    name: 'dashboard',
+    path: "/",
+    name: "dashboard",
     component: DashboardView,
     meta: {
-      requiresAuth: true
-    }
+      requiresAuth: true,
+    },
   },
   {
     path: "/login",
     name: "login",
     component: LoginView,
     meta: {
-      requiresAuth: false
-    }
+      requiresAuth: false,
+    },
   },
   {
     path: "/logout",
     name: "logout",
     component: LogoutView,
     meta: {
-      requiresAuth: false
-    }
+      requiresAuth: false,
+    },
   },
   {
     path: "/register",
     name: "register",
     component: RegisterView,
     meta: {
-      requiresAuth: false
-    }
+      requiresAuth: false,
+    },
   },
   {
     path: "/inventory",
     name: "inventory",
     component: InventoryView,
     meta: {
-      requiresAuth: true
-    }
-  }
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/picking",
+    name: "picking",
+    component: PickingView,
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/purchasing",
+    name: "purchasing",
+    component: PurchasingView,
+    meta: {
+      requiresAuth: true,
+    },
+  },
 ];
 
 // Create the router
 const router = createRouter({
   history: createWebHistory(),
-  routes: routes
+  routes: routes,
 });
 
 router.beforeEach((to) => {
-
   // Get the Vuex store
   const store = useStore();
 
   // Determine if the route requires Authentication
-  const requiresAuth = to.matched.some(x => x.meta.requiresAuth);
+  const requiresAuth = to.matched.some((x) => x.meta.requiresAuth);
 
   // If it does and they are not logged in, send the user to "/login"
-  if (requiresAuth && store.state.token === '') {
-    return {name: "login"};
+  if (requiresAuth && store.state.token === "") {
+    return { name: "login" };
   }
   // Otherwise, do nothing and they'll go to their next destination
 });
