c08290171801206d83471fb15436dc86af87fd3a
CraftyCurtis05 <craftycurtis05@gmail.com>
2024-02-06 12:17:25 -0500

    JLC: Stopped sidebar from pushing content

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div id="capstone-app" :style="{ 'margin-left': sidebarWidth }">
+   <div id="capstone-app" > <!--:style="{ 'margin-left': sidebarWidth }" -->
     <Sidebar />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha384-GLhlTQ8iMYY7eLq1GGem5uHvYRUFislFzL+8eFZ/woqm4u0FqFqBsJETqL8+8IEZ" crossorigin="anonymous">
-    <div id="nav">
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token !== ''">Logout</router-link>
-    </div>
-    <router-view />
+    <main id="main-view">
+      <div id="nav">
+        <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token !== ''">Logout</router-link>
+      </div>
+      <router-view />
+    </main>
   </div>
 </template>
 
@@ -27,5 +29,9 @@ export default {
   position: fixed;
   height: 100vh;
   width: 100vw;
+}
+
+#main-view {
+  margin-left: 56px;
 }
 </style>
